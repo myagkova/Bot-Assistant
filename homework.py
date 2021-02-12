7a97ab9498c384a9c48277dead264f55315e350b
Angelina Myagkova <aa.myagkova@gmail.com>
2021-02-12 15:38:05 +0100

    refactoring

--- homework.py
+++ homework.py
@@ -22,7 +22,8 @@ def parse_homework_status(homework):
     if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = ('Ревьюеру всё понравилось, '
+                   'можно приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
